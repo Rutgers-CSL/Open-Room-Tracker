9ea019856f3344c8ac0486f7a2872d90907e26ea
Farhan Khan <farhan.k0429@gmail.com>
2025-10-01 14:52:02 -0400

    added database connection

--- server/data/findrooms.py
+++ server/data/findrooms.py
@@ -7,11 +7,36 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup 
 #Time for timer at the end before closing
 import time
-#sqlite for storing data in database
-import sqlite3
+
+#mongodb for storing data in database
+from pymongo.mongo_client import MongoClient
+import certifi
+
+#Retrieve data from .env file
+import os
+
 #Datetime for converting time to 24 hour format
 import datetime
-import os
+
+
+#dotenv for monbodb uri
+from dotenv import load_dotenv
+
+load_dotenv()
+uri = os.getenv("MONGODB_URI")
+print(uri)
+
+
+
+# Create a new client and connect to the server
+client = MongoClient(uri, tlsCAFile=certifi.where())
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
  
 
 driver = webdriver.Chrome() 
